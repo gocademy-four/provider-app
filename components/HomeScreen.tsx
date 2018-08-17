be8fa90fe3089db3a1b6037ff1684f9c2ceaca5c
Daniel <nieltansah@gmail.com>
2018-08-17 23:27:31 +0700

    components: home: Fix logout button

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -2,26 +2,26 @@ import * as React from 'react';
 import { StyleSheet, View, AsyncStorage } from 'react-native';
 import { Text, Button } from 'react-native-elements';
 import { NavigationScreenProp } from 'react-navigation';
+import MainMenuCard from './MainMenuCard';
 
 interface Props {
   navigation: NavigationScreenProp<any>
 }
 
 export default class HomeScreen extends React.Component<Props> {
-  private logoutPressed = async () => {
-    await AsyncStorage.removeItem('token');
-    this.props.navigation.navigate('Initial');
-  }
-
-  static navigationOptions = {
-    headerTitle: 'Home',
-    headerLeft: (
-      <Button
-        onPress={this.logoutPressed}
-        title="Logout"
-      />
-    ),
-  };
+  static navigationOptions =
+    ({ navigation }: { navigation: NavigationScreenProp<any> }) => ({
+      headerTitle: 'Home',
+      headerLeft: (
+        <Button
+          onPress={async () => {
+            await AsyncStorage.removeItem('token');
+            navigation.navigate('Initial');
+          }}
+          title="Logout"
+        />
+      ),
+    });
 
   render() {
     return (
@@ -42,9 +42,6 @@ export default class HomeScreen extends React.Component<Props> {
           color='#88AA77'
           icon='history'
         />
-        <Button
-          title="Logout"
-          onPress={this.logoutPressed} />
       </View>
     );
   }
