379ad1bacde55ff285656fc2539dfd0f7e27cec3
Daniel <nieltansah@gmail.com>
2018-08-18 15:54:05 +0700

    components/SearchScreen: Fix dropdown

--- components/SearchScreen.tsx
+++ components/SearchScreen.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Alert, Platform } from 'react-native';
-import { Button } from 'react-native-elements';
-import { NavigationScreenProp } from 'react-navigation';
+import * as React from "react"
+import { Text, View, StyleSheet, Alert, Platform, ActivityIndicator } from "react-native"
+import { Button } from "react-native-elements"
+import { NavigationScreenProp } from "react-navigation"
 // @ts-ignore
-import { Picker } from 'react-native-picker-dropdown';
+import { Picker } from "react-native-picker-dropdown"
 
 interface Props {
   navigation: NavigationScreenProp<any>
@@ -16,93 +16,110 @@ interface State {
 
 export default class SearchScreen extends React.Component<Props, State> {
   constructor(props: Props, context?: any) {
-    super(props, context);
+    super(props, context)
 
     this.state = {
-      lesson: 'js',
-      lessonData: null,
-    };
+      lesson: "js",
+      lessonData: null
+    }
   }
 
   static navigationOptions = {
-    title: 'Search',
-  };
+    title: "Search"
+  }
 
-  private loginPressed = async () => {
+  private populateLessons = async () => {
     const response = await fetch(
-      "https://gocademy-tutor-api-server.herokuapp.com/classes");
+      "https://gocademy-tutor-api-server.herokuapp.com/classes",
+      {
+        method: "GET",
+        headers: {
+          Accept: "application/json"
+        }
+      }
+    )
 
-    this.setState({ lessonData: await response.json() })
+    if (response.status === 200) {
+      this.setState({ lessonData: await response.json() })
+    } else {
+      console.warn("Unable to fetch classes")
+    }
+  }
+
+  componentDidMount() {
+    this.populateLessons()
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Mulai pencarian!</Text>
-        <Text style={styles.subtitle}>Isi kolom berikut ini sesuai dengan kebutuhanmu.</Text>
-        <Picker
-          style={styles.picker}
-          selectedValue={this.state.lesson}
-          onValueChange={(lesson:string) => this.setState({ lesson })}
-          mode="dialog"
-          textStyle={styles.pickerText}
-        >
-        {
-          this.state.lessonData
-            ? this.state.lessonData.map(
-                (item, i) => <Picker.Item label={item} value={i} />)
-            : []
-        }
-        </Picker>
-
+        <Text style={styles.subtitle}>
+          Isi kolom berikut ini sesuai dengan kebutuhanmu.
+        </Text>
+        {this.state.lessonData ? (
+          <Picker
+            style={styles.picker}
+            selectedValue={this.state.lesson}
+            onValueChange={(lesson: string) => this.setState({ lesson })}
+            mode="dialog"
+            textStyle={styles.pickerText}
+          >
+            {this.state.lessonData.map((item, i) => (
+              <Picker.Item label={item} value={i} />
+            ))}
+          </Picker>
+        ) : (
+          <ActivityIndicator />
+        )}
         <Button
-          onPress={ () => this.props.navigation.navigate('SearchResult') }
+          onPress={() => this.props.navigation.navigate("SearchResult")}
           title="Cari Tutor"
-          icon={{name: 'search', color: 'white'}}
+          icon={{ name: "search", color: "white" }}
           buttonStyle={{
             borderRadius: 3,
-            backgroundColor: '#34495e'
+            backgroundColor: "#34495e"
           }}
           containerStyle={{
-            alignSelf: 'stretch',
-            marginTop: 12,
+            alignSelf: "stretch",
+            marginTop: 12
           }}
         />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     padding: 20,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1"
   },
   title: {
     fontSize: 24,
-    alignSelf: 'flex-start',
-    fontWeight: 'bold',
-    textAlign: 'justify',
-    color: '#34495e',
+    alignSelf: "flex-start",
+    fontWeight: "bold",
+    textAlign: "justify",
+    color: "#34495e"
   },
   subtitle: {
     marginTop: 14,
     fontSize: 18,
-    textAlign: 'justify',
-    color: '#34495e',
+    textAlign: "justify",
+    color: "#34495e"
   },
   picker: {
     marginTop: 14,
     paddingTop: 5,
     paddingBottom: 3,
     borderRadius: 3,
-    backgroundColor: 'white',
+    backgroundColor: "white"
   },
   pickerText: {
     padding: 10,
     color: "#34495e",
-    ...(Platform.OS === 'ios' ? { fontSize: 14 } : {})
+    ...(Platform.OS === "ios" ? { fontSize: 14 } : {})
   }
-});
+})
